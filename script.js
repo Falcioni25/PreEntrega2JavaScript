1d73945a74f739d8e868f8fdf1ce0df63a501e36
Facundo Lescano <eflescano@gmail.com>
2022-12-16 21:01:28 -0300

    Re-Entrega2JavaScript

--- script.js
+++ script.js
@@ -1,403 +1,150 @@
-const productos = [
-    {
-        nombre:"remera",
-        color:"rojo",
-        talle:"S",
-        precio: 1500,
-    },
-    {
-        nombre:"remera",
-        color:"verde",
-        talle:"M",
-        precio: 2500,
-    },
-    {
-        nombre:"remera",
-        color:"amarillo",
-        talle:"L",
-        precio: 3500,
-    },
-    {
-        nombre:"remera",
-        color:"Blanco",
-        talle:"XL",
-        precio: 4500,
-    },
-    {
-        nombre:"pantalon",
-        color:"mostaza",
-        talle:"S",
-        precio: 1500,
-    },
-    {
-        nombre:"pantalon",
-        color:"rosado",
-        talle:"M",
-        precio: 2500,
-    },
-    {
-        nombre:"pantalon",
-        color:"turquesa",
-        talle:"L",
-        precio: 3500,
-        
-    },
-    {
-        nombre:"pantalon",
-        color:"fucsia",
-        talle:"XL",
-        precio: 4500,   
-    },
-    {
-        nombre:"camisa",
-        color:"azul",
-        talle:"S",
-        precio: 1500,
-    },
-    {
-        nombre:"camisa",
-        color:"rojo",
-        talle:"M",
-        precio: 2500,
-    },
-    {
-        nombre:"camisa",
-        color:"beige",
-        talle:"L",
-        precio: 3500,
-    },
-    {
-        nombre:"camisa",
-        color:"salmon",
-        talle:"XL",
-        precio: 4500,
-    },
-    {
-        nombre:"medias",
-        color:"negro",
-        talle:"unico",
-        precio: 1500,
-    },
-    {
-        nombre:"medias",
-        color:"blanco",
-        talle:"unico",
-        precio: 1500,
-    },
-    {
-        nombre:"gorra",
-        color:"negro",
-        talle:"unico",
-        precio: 1500,
-    },
+const autos = [ {
+  marca: "ford",
+  color: "rojo",
+  modelo: "mondeo",
+  anio: 1998,
+  precio: 150000,  
+},
+{
+  marca: "ford",
+  color: "verde",
+  modelo: "mondeo",
+  anio: 1998,
+  precio: 150000,  
+},
+{
+  marca: "ford",
+  color: "azul",
+  modelo: "sierra",
+  anio: 1995,
+  precio: 180000     
+},
+{
+  marca: "ford",
+  color: "Amarillo",
+  modelo: "falcon",
+  anio: 1995,
+  precio: 200000,       
+},
+{
+  marca: "ford",
+  color: "Verde",
+  modelo: "taunus",
+  anio: 1995,
+  precio: 220000,       
+},
+{
+  marca: "chevrolet",
+  color: "azul",
+  modelo: "camaro",
+  anio: 2018,
+  precio: 250000,      
+},
+{
+  marca: "chevrolet",
+  color: "gris",
+  modelo: "onix",
+  anio: 2015,
+  precio: 300000,      
+},
+{
+  marca: "chevrolet",
+  color: "Blanco",
+  modelo: "prisma",
+  anio: 2019,
+  precio: 150000,       
+},
+{
+  marca: "chevrolet",
+  color: "Negro",
+  modelo: "corsa",
+  anio: 2008,
+  precio: 500000,       
+}
 ]
-
-
-alert("Bienvenidos a Tienda OnLine \nContamos con la mas surtida variedad de productos en:\nRemeras\nPantalones\nCamisas\n")
-
-
-var productoElegido = Number(prompt("Por favor ingrese la opcion del producto que desea comprar\n 1-Remeras\n 2-Pantalones\n 3-Camisas\n-4 Salir\n"))
-
-if (productoElegido == 1){
-
-let salida = productos.filter(producto => producto.nombre === "remera")
-
-let seleccion = ""
-
-for (const producto of salida) {
-  
-  seleccion = seleccion + producto.nombre+" "+producto.talle+" "+producto.precio+"\n" 
+//-------------------------------FUNCION DECLARADA-----------------------------------------
+function venderMiAuto (){
+  let marca = prompt("Ingrese la marca de su vehiculo");
+  let color = prompt("Ingrese el color de su vehiculo");
+  let modelo = prompt("Ingrese el modelo de su vehiculo");
+  let anio= prompt("Ingrese el año de fabricacion de su vehiculo");
+  let precio = prompt("Ingrese monto pretendido por su vehiculo");
+  let auto = new Auto(marca, color, modelo, anio,precio);
+  autos.push(auto);
+  alert("Auto Agregado:"+"\n"+ "Marca :" + auto.marca+"\n"+ "Color: " +auto.color+"\n"+ "Modelo :" + auto.modelo+"\n"+"Año :" +auto.anio+"\n"+"Precio $: "+ auto.precio+".")
+}
+//-----------------------FUNCION CONSTRUCTORA------------------------------------------------
+function Auto(marca, color, modelo, anio, precio){
+  this.marca = marca;
+  this.color = color;
+  this.modelo = modelo;
+  this.anio = anio;
+  this.precio = precio;   
+}
+//------------------------------------------INICIO-----------------------------------------------------
+alert("Bienvenidos a Automotores Usados Facundo")
+let eleccion1 = Number(prompt("Ingrese la opcion de la marca que le interese.\n1-Ford\n2-Chevrolet\n3-Si le interesa vender su usado"))
+if(eleccion1 == 1){
+let eleccionMarca = autos.filter((auto)=>auto.marca === "ford")
+//--------------------------------------CONSOLE.LOG []-------------------------------------------
+let salida =[]
+for (const auto of eleccionMarca) {
+salida.push(auto)
 }
 console.log(salida)
-
-const seleccion2 = Number(prompt("Elija la opcion del producto deseado:\n1-Talle S\n2-Talle M\n3-Talle L\n4-Talle XL"))
-
-if(seleccion2 == 1){
-  //let carrito = seleccion.filter(producto => producto.talle === "S")
-  const chango = []
-  chango.push(salida[0])
-  
-  console.log(chango)
-
-  let seleccion3 = prompt("Desea hacer otra compra?\1-Si\2-No")
-
-  if(seleccion3 == 1){
-    var productoElegido = Number(prompt("Por favor ingrese la opcion del producto que desea comprar\n 1-Remeras\n 2-Pantalones\n 3-Camisas\n-4 Salir\n"))
-
-  }else{
-    let salida2 = ""
-    for (const elemento of chango) {
-      salida2 = salida2 + elemento.nombre+" talle "+elemento.talle+" "+elemento.precio
-    }
-    alert("Usted ha comprado:"+ " " + salida2+"."+"\n"+"Gracias por comprar en Tienda Online")
-    
-    }
-     
-  }
-    else if(seleccion2 == 2){
-  //let carrito = seleccion.filter(producto => producto.talle === "S")
-  const chango = []
-  chango.push(salida[1])
-  
-  console.log(chango)
-
-  let seleccion3 = prompt("Desea hacer otra compra?\1-Si\2-No")
-
-  if(seleccion3 == 1){
-    var productoElegido = Number(prompt("Por favor ingrese la opcion del producto que desea comprar\n 1-Remeras\n 2-Pantalones\n 3-Camisas\n-4 Salir\n"))
-
-  }else{
-    let salida2 = ""
-    for (const elemento of chango) {
-      salida2 = salida2 + elemento.nombre+" talle "+elemento.talle+" "+elemento.precio
-    }
-    alert("Usted ha comprado:"+ " " + salida2+"."+"\n"+"Gracias por comprar en Tienda Online")
-    
-    }
-     
-  }
-    else if (productoElegido == 2){
-
-    let salida = productos.filter(producto => producto.nombre === "pantalon")
-    
-    let seleccion = ""
-    
-    for (const producto of salida) {
-      
-      seleccion = seleccion + producto.nombre+" "+producto.talle+" "+producto.color + " "+ producto.precio+"\n" 
-    }
-    console.log(salida)
-    
-    let seleccion2 = Number(prompt("Elija la opcion del producto deseado:\n1-Talle S\n2-Talle M\n3-Talle L\n4-Talle XL"))
-    
-    if(seleccion2 == 1){
-      //let carrito = seleccion.filter(producto => producto.talle === "S")
-      const chango = []
-      chango.push(salida[0])
-      
-      console.log(chango)
-    
-      let seleccion3 = prompt("Desea hacer otra compra?\1-Si\2-No")
-    
-      if(seleccion3 == 1){
-        var productoElegido = Number(prompt("Por favor ingrese la opcion del producto que desea comprar\n 1-Remeras\n 2-Pantalones\n 3-Camisas\n-4 Salir\n"))
-    
-      }else{
-        let salida2 = ""
-        for (const elemento of chango) {
-          salida2 = salida2 + elemento.nombre+" color "+elemento.color+" talle "+elemento.talle+" "+ "$ " + elemento.precio
-        }
-        alert("Usted ha comprado:"+ " " + salida2+"."+"\n"+"Gracias por comprar en Tienda Online")
-        
-        }
-         
-      }
-  
-    }
-    /* else if (productoElegido == 3){
-
-      let salida = productos.filter(producto => producto.nombre === "camisa")
-      
-      let seleccion = ""
-      
-      for (const producto of salida) {
-        
-        seleccion = seleccion + producto.nombre+" "+producto.talle+" "+producto.color + " "+ producto.precio+"\n" 
-      }
-      console.log(salida)
-      
-      let seleccion2 = Number(prompt("Elija la opcion del producto deseado:\n1-Talle S\n2-Talle M\n3-Talle L\n4-Talle XL"))
-      
-      if(seleccion2 == 1){
-        //let carrito = seleccion.filter(producto => producto.talle === "S")
-        const chango = []
-        chango.push(salida[0])
-        
-        console.log(chango)
-      
-        let seleccion3 = prompt("Desea hacer otra compra?\1-Si\2-No")
-      
-        if(seleccion3 == 1){
-          var productoElegido = Number(prompt("Por favor ingrese la opcion del producto que desea comprar\n 1-Remeras\n 2-Pantalones\n 3-Camisas\n-4 Salir\n"))
-      
-        }else{
-          let salida2 = ""
-          for (const elemento of chango) {
-            salida2 = salida2 + elemento.nombre+" color "+elemento.color+" talle "+elemento.talle+" "+ "$ " + elemento.precio
-          }
-          alert("Usted ha comprado:"+ " " + salida2+"."+"\n"+"Gracias por comprar en Tienda Online")
-          
-          }
-           
-        }
-    
-      } */
+//---------------------------------------ALERT PARA MOSTRAR POR PANTALLA ""-----------------------
+let salida2 = ""
+for (const auto of eleccionMarca){
+salida2 = salida2 + " " + auto.modelo + " " +   auto.color + " " + " " + auto.anio + " " + auto.precio+ " ." + "\n"}
+//----------------------------------ELECCION DE MODELO------------(FORD)------------------------------
+let eleccion2 = prompt(salida2 + "\n"+ "Ingrese el modelo de usado que le interese:").toLowerCase();
+const usadoElegido = salida.filter(auto => auto.modelo == eleccion2);
+let salida3 = []
+let salida4 = ""   
+//------------------------------------CONSOLE.LOG []-----(FORD)--------------------------------------------
+for (const auto of usadoElegido) {
+  salida3.push(auto)
 }
-
-  
-
-
-
-
-
-
-  /* let talleElegido = prompt("Por favor elija el talle que desea comprar\n1-Talle S\n2-Talle M\n3-Talle L\n4-Talle XL")
-
-  if(talleElegido == 1){
-    carrito.push(eleccion[0])
-  }
-  else if (talleElegido == 2){
-    carrito.push(eleccion[1])
-  }
-  else if (talleElegido == 3){
-    carrito.push(eleccion[2])
-  }
-  else{
-    carrito.push(eleccion[3])
-  }
-
-  console.log(Object.values(carrito))
-   */
-  
-/* else if (productoElegido == 2){
-
-  let salida = productos.filter(producto => producto.nombre === "pantalon")
-  const eleccion = []
-  const carrito = []
-  for (const producto of salida) {
-    eleccion.push(producto)
-  }  
-    let talleElegido = prompt("Por favor elija el talle que desea comprar\n1-Talle S\n2-Talle M\n3-Talle L\n4-Talle XL")
-  
-    if(talleElegido == 1){
-      carrito.push(eleccion[0])
-    }
-    else if (talleElegido == 2){
-      carrito.push(eleccion[1])
-    }
-    else if (talleElegido == 3){
-      carrito.push(eleccion[2])
-    }
-    else{
-      carrito.push(eleccion[3])
-    }
-    console.log(carrito)
-  } else if (productoElegido == 3){
-
-    let salida = productos.filter(producto => producto.nombre === "camisa")
-    const eleccion = []
-    const carrito = []
-    for (const producto of salida) {
-      eleccion.push(producto)
-    }  
-      let talleElegido = prompt("Por favor elija el talle que desea comprar\n1-Talle S\n2-Talle M\n3-Talle L\n4-Talle XL")
-    
-      if(talleElegido == 1){
-        carrito.push(eleccion[0])
-      }
-      else if (talleElegido == 2){
-        carrito.push(eleccion[1])
-      }
-      else if (talleElegido == 3){
-        carrito.push(eleccion[2])
-      }
-      else{
-        carrito.push(eleccion[3])
-      }
-      console.log(carrito)
-    } else{
-      alert("usted eligio una opcion incorrecta")
-    } */
-/* else if (productoElegido == 2){
-
-  let salida = productos.filter(producto => producto.nombre === "pantalon")
-  const eleccion = []
-  for (const producto of salida) {
-    eleccion.push(producto) 
-  }
-  console.log(eleccion)
-  }
-else if (productoElegido == 3){
-
-  let salida = productos.filter(producto => producto.nombre === "medias")
-  const eleccion = []
-  for (const producto of salida) {
-    eleccion.push(producto) 
-  }
-  console.log(eleccion)
-  }
-else if (productoElegido == 4){
-
-  let salida = productos.filter(producto => producto.nombre === "gorra")
-  const eleccion = []
-  for (const producto of salida) {
-    eleccion.push(producto) 
-  }
-  console.log(eleccion)
-  }
-  else{
-    alert("Usted eligio una opcion incorrecta")
-  } */
-
-
-/* else if (productoElegido == 2) {
-
-  let salida = productos.filter(producto => producto.nombre === "pantalon")
-
-for (const producto of salida) {
-   eleccion = eleccion + " " + producto.nombre + " " + "color" + " " +  producto.color + " " + "talle" + " " + producto.talle + " " + " $ " + producto.precio + "\n"
-  
+console.log(salida3)
+//----------------------------------ALERT PARA MOSTRAR POR PANTALLA ""---(FORD)----------------------------
+for (const auto of usadoElegido) {
+  salida4= salida4 + "Usted eligió " + auto.modelo.charAt(0).toUpperCase() + auto.modelo.slice(1) + " color " + auto.color + " que vale $ " + auto.precio + "\n"}
+  alert(salida4)
+//-----------------------------------SI EL CLIENTE ELIGE CHEVROLET-------------------------------------------------------
+}else if(eleccion1 == 2){
+let eleccionMarca = autos.filter((auto)=>auto.marca === "chevrolet")
+//------------------------------------CONSOLE.LOG []----(CHEVROLET)--------------------------------------------
+let salida3 =[]
+for (const auto of eleccionMarca) {
+  salida3.push(auto)
 }
-} else if (productoElegido == 3) {
-  let salida = productos.filter(producto => producto.nombre === "medias")
-
-  const eleccion = ""
-
-for (const producto of salida) {
-   eleccion = eleccion + " " + producto.nombre + " " + "color" + " " +  producto.color + " " + "talle" + " " + producto.talle + " " + " $ " + producto.precio + "\n"
-  
+console.log(salida3)
+//-----------------------------------ALERT "" PARA MOSTRAR POR PANTALLA --(CHEVROLET)---------------------------
+let salida4 = ""
+for (const auto of eleccionMarca) {
+salida4 = salida4 + " " + " " +   auto.modelo + " "  + auto.color + " " + auto.anio + " " + auto.precio+ " ." + "\n"
 }
-  
-} else {
-  let salida = productos.filter(producto => producto.nombre ==="gorra")
-
-const eleccion = ""
-
-for (const producto of salida) {
-   eleccion = eleccion + " " + producto.nombre + " " + "color" + " " +  producto.color + " " + "talle" + " " + producto.talle + " " + " $ " + producto.precio + "\n"
-  
-} 
+//----------------------------------ELECCION DE MODELO--------(CHEVROLET)-----------------------
+let eleccion2 = prompt(salida4 + "\n"+ "Ingrese el modelo de usado que le interese:").toLowerCase();
+let usadoElegido2 = salida3.filter(auto => auto.modelo == eleccion2);
+let salida5 = [""]
+let salida6 = ""
+//------------------------------------CONSOLE.LOG []--------------------------------------------
+for (const auto of usadoElegido2) {
+  salida5.push(auto)
 }
-
-alert(eleccion)
-
-
-
-let eleccion2 = prompt("Que articulo desea comprar? Por favor copie en el campo correspondiente la descripcion del producto y el precio : \n" + eleccion)
-
-let carrito = []
-
-carrito.push(eleccion2);
-
-console.log(carrito)
-
-alert("usted cargó en su carrito el siguiente producto:\n" + carrito)
-
-var eleccion3 = Number(prompt("Desea hacer alguna compra mas? \n 1-SI\n2-NO"))
-
-if(eleccion3 == 1){
-var productoElegido = Number(prompt("Por favor ingrese la opcion del producto que desea comprar\n 1-Remeras\n 2-Pantalones\n 3-Medias \n 4 Gorras"))
-let salida = productos.filter(producto => producto.nombre === "remera")
-
-const eleccion = []
-
-for (const producto of salida) {
-   eleccion = eleccion + " " + producto.nombre + " " + "color" + " " +  producto.color + " " + "talle" + " " + producto.talle + " " + " $ " + producto.precio + "\n"
+console.log(salida5)
+//----------------------------------ALERT ""  PARA MOSTRAR POR PANTALLA ----------------------------
+for (const auto of usadoElegido2) {
+  salida6= salida6 + " " + auto.modelo.charAt(0).toUpperCase() + auto.modelo.slice(1) + " " + auto.color + " " + auto.precio + "\n"}
+  alert(salida6)
 }
+//-----------------------------------PARA AGREGAR UN AUTO PARA LA VENTA-------------------------------------------------
+else{
+venderMiAuto();
+alert("Su auto se agregó correctamente. Gracias por publicar en Automotores Facundo")
+stock = ""
+autos.forEach(auto=>
+stock=stock +" "+ auto.marca.charAt(0).toUpperCase() + auto.marca.slice(1) + " "+ auto.modelo.charAt(0).toUpperCase() + auto.modelo.slice(1)+" $ "+auto.precio+"\n" )
+alert("Nuestra flota de autos es : \n" + stock+"\n"+"Lo esperamos nuevamente")  
 }
-  else{
-    const total = eleccion.reduce((acumulador,precio) => acumulador + numero, 0)
-    
-  }
-  alert("Su compra es un total de :" + total )
-  */
